81a8cf390a45de39b689411771f9d84d4cb29e1c
KINGMJ <328047478@qq.com>
2021-04-19 16:29:29 +0800

    配置ExMachina

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Conduit.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:commanded, "~> 1.2"},
-      {:commanded_eventstore_adapter, "~> 1.2"}
+      {:commanded_eventstore_adapter, "~> 1.2"},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
