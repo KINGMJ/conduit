bf1746392618f8401683a5f1edc60673daea11b9
KINGMJ <328047478@qq.com>
2021-04-19 21:20:35 +0800

    修改测试环境 db 配置

--- config/test.exs
+++ config/test.exs
@@ -8,16 +8,6 @@ config :conduit, Conduit.Repo,
   hostname: "localhost",
   pool_size: 10
 
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
-config :conduit, Conduit.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "conduit_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :conduit, ConduitWeb.Endpoint,
