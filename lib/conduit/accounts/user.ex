df07a98709c9f82d82b45fd81a240b941969e029
KINGMJ <328047478@qq.com>
2021-04-25 18:03:38 +0800

    user schema 新增changeset，对username和email做约束

--- lib/conduit/accounts/user.ex
+++ lib/conduit/accounts/user.ex
@@ -1,5 +1,6 @@
 defmodule Conduit.Accounts.User do
   use Ecto.Schema
+  import Ecto.Changeset
 
   @primary_key {:uuid, :binary_id, autogenerate: false}
 
@@ -12,4 +13,15 @@ defmodule Conduit.Accounts.User do
 
     timestamps()
   end
+
+  def changeset(user, params) do
+    user
+    |> cast(params, [:uuid, :email, :username, :bio, :hashed_password, :image])
+    |> validate_required([:uuid, :email, :username])
+    |> unique_constraint(:email, name: :accounts_users_email_index)
+    |> unique_constraint(:username, name: :accounts_users_username_index)
+    |> unique_constraint([:email, :username])
+  end
+
+  def new_changeset(attrs), do: changeset(%__MODULE__{}, attrs)
 end
