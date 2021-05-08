889cd0341c41336447d447cd3aa166ce6338086e
KINGMJ <328047478@qq.com>
2021-05-08 17:50:17 +0800

    使用command作为event的样板代码

--- lib/conduit/accounts/events/user_registered.ex
+++ lib/conduit/accounts/events/user_registered.ex
@@ -1,9 +1,12 @@
 defmodule Conduit.Accounts.Events.UserRegistered do
-  @derive Jason.Encoder
-  defstruct [
-    :user_uuid,
-    :username,
-    :email,
-    :hashed_password
-  ]
+  use Commanded.Event,
+    from: Conduit.Accounts.Commands.RegisterUser
+
+  # @derive Jason.Encoder
+  # defstruct [
+  #   :user_uuid,
+  #   :username,
+  #   :email,
+  #   :hashed_password
+  # ]
 end
