36163f1cd4bf176eabbc1e259eccf0f40a12801a
KINGMJ <328047478@qq.com>
2021-05-08 17:41:34 +0800

    取消注释

--- lib/conduit/accounts/accounts.ex
+++ lib/conduit/accounts/accounts.ex
@@ -19,13 +19,11 @@ defmodule Conduit.Accounts do
       |> assign(:user_uuid, uuid)
       |> RegisterUser.new()
 
-    IO.inspect(register_user)
-    IO.inspect(register_user.valid?)
-
     unless register_user.valid? do
       {:error, register_user.errors}
     else
       cmd = Ecto.Changeset.apply_changes(register_user)
+
       with :ok <- CommandedApp.dispatch(cmd, consistency: :strong) do
         get(User, uuid)
       else
