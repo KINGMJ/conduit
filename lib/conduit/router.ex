40ed409e8830f8fbc28ef5805c50f6609fe1756b
KINGMJ <328047478@qq.com>
2021-05-19 13:50:24 +0800

    router中加入Uniqueness中间件

--- lib/conduit/router.ex
+++ lib/conduit/router.ex
@@ -3,6 +3,9 @@ defmodule Conduit.Router do
 
   alias Conduit.Accounts.Aggregates.User
   alias Conduit.Accounts.Commands.RegisterUser
+  alias Conduit.Support.Middleware.Uniqueness
+
+  middleware(Uniqueness)
 
   dispatch([RegisterUser], to: User, identity: :user_uuid)
 end
