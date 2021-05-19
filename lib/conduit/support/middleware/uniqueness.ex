da7402913d91f8075f9375dbfd2774395e6df107
KINGMJ <328047478@qq.com>
2021-05-19 13:51:05 +0800

    新增uniqueness中间件

---
+++ lib/conduit/support/middleware/uniqueness.ex
@@ -0,0 +1,61 @@
+defmodule Conduit.Support.Middleware.Uniqueness do
+  @behaviour Commanded.Middleware
+
+  alias Conduit.Accounts.Commands.RegisterUser
+  alias Conduit.Support.Unique
+  alias Commanded.Middleware.Pipeline
+  import Pipeline
+
+  @doc """
+  实现一个协议
+  """
+  defprotocol UniqueFields do
+    @fallback_to_any true
+    @doc "Returns unique fields for the command"
+    def unique(command)
+  end
+
+  defimpl UniqueFields, for: Any do
+    def unique(_command), do: []
+  end
+
+  defimpl UniqueFields, for: RegisterUser do
+    def unique(_command),
+      do: [
+        {:username, {"has already been taken"}}
+      ]
+  end
+
+  def before_dispatch(%Pipeline{command: command} = pipeline) do
+    case ensure_uniqueness(command) do
+      :ok ->
+        pipeline
+
+      {:error, errors} ->
+        pipeline
+        |> respond({:error, errors})
+        |> halt()
+    end
+  end
+
+  def after_dispatch(pipeline), do: pipeline
+  def after_failure(pipeline), do: pipeline
+
+  defp ensure_uniqueness(command) do
+    command
+    |> UniqueFields.unique()
+    |> Enum.reduce_while(:ok, fn {unique_field, error_message}, _ ->
+      value = Map.get(command, unique_field)
+
+      case Unique.claim(unique_field, value) do
+        :ok ->
+          IO.inspect("success")
+          {:cont, :ok}
+
+        {:error, :already_taken} ->
+          IO.inspect("error")
+          {:halt, {:error, Keyword.new([{unique_field, error_message}])}}
+      end
+    end)
+  end
+end
