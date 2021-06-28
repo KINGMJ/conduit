8a1717a4683cbf4797c01be403bc8ab8ae174a81
KINGMJ <328047478@qq.com>
2021-06-28 17:46:21 +0800

    提供一个article_factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -13,6 +13,17 @@ defmodule Conduit.Factory do
     }
   end
 
+  def article_factory do
+    %{
+      slug: "how-to-train-your-dragon",
+      title: "How to train your dragon",
+      description: "Ever wonder how?",
+      body: "You have to believe",
+      tag_list: ["dragons", "training"],
+      author_uuid: UUID.uuid4()
+    }
+  end
+
   def register_user_factory do
     struct(RegisterUser, build(:user))
   end
