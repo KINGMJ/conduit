9d38c18cd06ee5268e62335a59e715923f2b9048
KINGMJ <328047478@qq.com>
2021-07-05 16:11:14 +0800

    新增PublishArticle commands

---
+++ lib/conduit/blog/commands/publish_article.ex
@@ -0,0 +1,48 @@
+defmodule Conduit.Blog.Commands.PublishArticle do
+  use Commanded.Command,
+    article_uuid: :binary_id,
+    author_uuid: :binary_id,
+    slug: :string,
+    title: :string,
+    description: :string,
+    body: :string,
+    tag_list: {{:array, :string}, []}
+
+  alias Conduit.Blog.Projections.Author
+  alias Conduit.Blog.Slugger
+
+  def handle_validate(changeset) do
+    changeset
+    |> validate_required([
+      :article_uuid,
+      :author_uuid,
+      :slug,
+      :title,
+      :description,
+      :body,
+      :tag_list
+    ])
+  end
+
+  @doc """
+  Assign a unique identity for the article
+  """
+  def assign_uuid(attrs, uuid), do: Map.put(attrs, :article_uuid, uuid)
+
+  @doc """
+  Assign the author
+  """
+  def assign_author(attrs, %Author{uuid: uuid}) do
+    %{attrs | author_uuid: uuid}
+  end
+
+  @doc """
+  Generate a unique URL slug from the article title
+  """
+  def generate_url_slug(%{title: title} = attrs) do
+    case Slugger.slugify(title) do
+      {:ok, slug} -> %{attrs | slug: slug}
+      _ -> attrs
+    end
+  end
+end
