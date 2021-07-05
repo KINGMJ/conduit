0a96b50ffe16c8d89e16f358a8b328b9436d9b23
KINGMJ <328047478@qq.com>
2021-07-05 16:08:00 +0800

    article的建表语句和schema调整

--- lib/conduit/blog/projections/article.ex
+++ lib/conduit/blog/projections/article.ex
@@ -2,15 +2,17 @@ defmodule Conduit.Blog.Projections.Article do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:uuid, :binary_id, autogenerate: false}
+
   schema "blog_articles" do
     field :author_bio, :string
     field :author_image, :string
     field :author_username, :string
-    field :author_uuid, :binary
+    field :author_uuid, :binary_id
     field :body, :string
     field :description, :string
-    field :favorite_count, :integer
-    field :published_at, :naive_datetime
+    field :favorite_count, :integer, default: 0
+    field :published_at, :utc_datetime_usec
     field :slug, :string
     field :tag_list, {:array, :string}
     field :title, :string
@@ -22,6 +24,7 @@ defmodule Conduit.Blog.Projections.Article do
   def changeset(article, attrs) do
     article
     |> cast(attrs, [
+      :uuid,
       :slug,
       :title,
       :description,
@@ -35,6 +38,7 @@ defmodule Conduit.Blog.Projections.Article do
       :author_image
     ])
     |> validate_required([
+      :uuid,
       :slug,
       :title,
       :description,
@@ -47,5 +51,6 @@ defmodule Conduit.Blog.Projections.Article do
       :author_bio,
       :author_image
     ])
+    |> unique_constraint(:slug)
   end
 end
--- priv/repo/migrations/20210624033543_create_blog_articles.exs
+++ priv/repo/migrations/20210624033543_create_blog_articles.exs
@@ -2,14 +2,15 @@ defmodule Conduit.Repo.Migrations.CreateBlogArticles do
   use Ecto.Migration
 
   def change do
-    create table(:blog_articles) do
+    create table(:blog_articles, primary_key: false) do
+      add :uuid, :uuid, primary_key: true
       add :slug, :text
       add :title, :text
       add :description, :text
       add :body, :text
       add :tag_list, {:array, :text}
       add :favorite_count, :integer
-      add :published_at, :naive_datetime
+      add :published_at, :utc_datetime_usec
       add :author_uuid, :binary
       add :author_username, :text
       add :author_bio, :text
@@ -18,5 +19,8 @@ defmodule Conduit.Repo.Migrations.CreateBlogArticles do
       timestamps()
     end
 
+    # 创建唯一索引
+    create unique_index(:blog_articles, [:slug])
+    create index(:blog_articles, [:author_uuid,:author_username,:published_at])
   end
 end
