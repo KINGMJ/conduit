269c20a2d747b1ca2518c06f916763119fd1c8f5
KINGMJ <328047478@qq.com>
2021-07-05 17:49:44 +0800

    新增一个检验slug唯一性的测试用例

--- test/conduit/blog/blog_test.exs
+++ test/conduit/blog/blog_test.exs
@@ -10,7 +10,23 @@ defmodule Conduit.BlogTest do
     @tag :integration
     test "should succeed with valid data", %{author: author} do
       assert {:ok, %Article{} = article} = Blog.publish_article(author, build(:article))
-      IO.inspect(article)
+      assert article.slug == "how-to-train-your-dragon"
+      assert article.title == "How to train your dragon"
+      assert article.description == "Ever wonder how?"
+      assert article.body == "You have to believe"
+      assert article.tag_list == ["dragons", "training"]
+      assert article.author_username == "jake"
+      assert article.author_bio == nil
+      assert article.author_image == nil
+    end
+
+    @tag :integration
+    test "should generate unique URL slug", %{author: author} do
+      assert {:ok, %Article{} = article1} = Blog.publish_article(author, build(:article))
+      assert article1.slug == "how-to-train-your-dragon"
+
+      assert {:ok, %Article{} = article2} = Blog.publish_article(author, build(:article))
+      assert article2.slug == "how-to-train-your-dragon-2"
     end
   end
 
