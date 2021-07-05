20eb8189b18d399793796d9643d37283554f7d3e
KINGMJ <328047478@qq.com>
2021-07-05 16:10:57 +0800

    新增article aggregates

---
+++ lib/conduit/blog/aggregates/article.ex
@@ -0,0 +1,40 @@
+defmodule Conduit.Blog.Aggregates.Article do
+  defstruct [
+    :uuid,
+    :author_uuid,
+    :slug,
+    :title,
+    :description,
+    :body,
+    :tag_list
+  ]
+
+  alias Conduit.Blog.Commands.PublishArticle
+  alias Conduit.Blog.Events.ArticlePublished
+  alias __MODULE__
+
+  def execute(%Article{uuid: nil}, %PublishArticle{} = publish) do
+    %ArticlePublished{
+      article_uuid: publish.article_uuid,
+      slug: publish.slug,
+      title: publish.title,
+      description: publish.description,
+      body: publish.body,
+      tag_list: publish.tag_list,
+      author_uuid: publish.author_uuid
+    }
+  end
+
+  def apply(%Article{} = article, %ArticlePublished{} = published) do
+    %Article{
+      article
+      | uuid: published.article_uuid,
+        slug: published.slug,
+        title: published.title,
+        description: published.description,
+        body: published.body,
+        tag_list: published.tag_list,
+        author_uuid: published.author_uuid
+    }
+  end
+end
